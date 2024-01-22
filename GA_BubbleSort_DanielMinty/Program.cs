1703fdfc71a933805a9f51e747a39c2d5b0009b9
Daniel Minty <60455996+Dminty87@users.noreply.github.com>
2024-01-22 15:50:23 -0800

    Programming functional

--- GA_BubbleSort_DanielMinty/Program.cs
+++ GA_BubbleSort_DanielMinty/Program.cs
@@ -10,6 +10,60 @@ namespace GA_BubbleSort_DanielMinty
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+
+            int[] ints = new int[rand.Next(10, 31)];
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                ints[i] = rand.Next(1, 100);
+            }
+
+            foreach (int i in ints)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            BubbleSort(ints);
+
+            foreach (int i in ints)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+
         }
+
+        public static void BubbleSort(int[] array)
+        {
+            bool swapOccured = true;
+
+            while (swapOccured)
+            {
+                swapOccured = false;
+
+                for (int currentIndex = 0; currentIndex < array.Length - 1; currentIndex++)
+                {
+                    if (array[currentIndex] > array[currentIndex + 1])
+                    {
+                        int temp = array[currentIndex + 1];
+
+                        array[currentIndex + 1] = array[currentIndex];
+
+                        array[currentIndex] = temp;
+
+                        swapOccured = true;
+
+                    }
+
+                }
+
+            }
+
+        }
+
     }
 }
